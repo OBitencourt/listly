dde3b71a433b26dd9003398df4afa5fb9cbd8822
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-10-21 17:04:12 +0000

    feat: configuring initial server structure

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// requisições base
+
+const express = require('express')
+const cors = require('cors')
+
+// requisições criadas
+
+// mongodb
+// rotas
+
+//
+
+const app = express()
+
+
+app.use(cors())
+
+// configuring server to receive datas in json
+
+app.use(express.json())
+
+// 404 error (not found)
+
+app.use('/', (req, res) => {
+    res.send('Página não encontrada!')
+})
+
+
+// server listening
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
