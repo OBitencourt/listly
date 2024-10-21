9bf4090df58fd3813ad9b13ee103560a20603c6c
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-10-21 17:38:19 +0000

    feat: adding initial tasks model

---
+++ backend/models/tasks.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose')
+
+const TaskSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true, // O campo 'task' é obrigatório
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+      /*
+    listId: {
+        type: Schema.Types.ObjectId, 
+        ref: 'List', // Referência ao model da lista
+        required: true,
+    },*/
+})
+
+const TasksModel = mongoose.model('tasks', TaskSchema)
+
+module.exports = TasksModel
